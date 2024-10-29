f1b709e33c5f711af9744965b79c30becd038837
Martin Kosturkov <martin.kosturkovv@gmail.com>
2024-10-29 22:57:17 +0200

    Separate kafka topic configurations

--- notification-processor/src/main/java/com/mkosturkov/notificationprocessor/config/KafkaConfig.java
+++ notification-processor/src/main/java/com/mkosturkov/notificationprocessor/config/KafkaConfig.java
@@ -4,20 +4,31 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
 
 @Configuration
 public class KafkaConfig {
 
     @Bean
-    public KafkaAdmin.NewTopics emailNotificationsTopic(
-            @Value("${kafka.topics.email-notifications}") String emailNotificationsTopic,
-            @Value("${kafka.topics.sms-notifications}") String smsNotificationsTopic,
-            @Value("${kafka.topics.slack-notifications}") String slackNotificationsTopic) {
+    public NewTopic emailNotificationsTopic(
+            @Value("${kafka.topics.email-notifications.name}") String topicName,
+            @Value("${kafka.topics.email-notifications.partitions}") Integer partitions) {
 
-        return new KafkaAdmin.NewTopics(
-                new NewTopic(emailNotificationsTopic, 5, (short) 1),
-                new NewTopic(smsNotificationsTopic, 5, (short) 1),
-                new NewTopic(slackNotificationsTopic, 5, (short) 1));
+        return new NewTopic(topicName, partitions, (short) 1);
+    }
+
+    @Bean
+    public NewTopic smsNotificationsTopic(
+            @Value("${kafka.topics.sms-notifications.name}") String topicName,
+            @Value("${kafka.topics.sms-notifications.partitions}") Integer partitions) {
+
+        return new NewTopic(topicName, partitions, (short) 1);
+    }
+
+    @Bean
+    public NewTopic slackNotificationsTopic(
+            @Value("${kafka.topics.slack-notifications.name}") String topicName,
+            @Value("${kafka.topics.slack-notifications.partitions}") Integer partitions) {
+
+        return new NewTopic(topicName, partitions, (short) 1);
     }
 }
